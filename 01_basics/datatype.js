15e5e63c535b007a3d4c5e66c13a8f0ce01e16c6
Krish7Hack <145795573+Krish7Hack@users.noreply.github.com>
2024-05-29 05:52:09 +0000

    commit config change

---
+++ 01_basics/datatype.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version.
+
+//alert(3+3)//we are using nodejs and not browser.
+
+console.log(3+3);//code readability should be high.
+
+console.log("Krishna");
+
+let name="krishna";
+let age=20;
+let isLoggedIn=false;
+
+/*
+primitive data type
+*/
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null --> standalone value
+//undefined
+//symbol-->more implementation in react.
+
+console.log(typeof age);
+
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
