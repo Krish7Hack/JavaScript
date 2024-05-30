8d972c467bafc6c30f6448466e9ec1d0def60547
Krish7Hack <145795573+Krish7Hack@users.noreply.github.com>
2024-05-30 07:38:44 +0000

    conversion of data types

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,26 @@
+let score = undefined;
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1
+
+let booleanIslooggedIn=Boolean(isLoggedIn);
+console.log(booleanIslooggedIn);//true
+
+//1 = >true ; 0=>false
+//""=>false
+//"krishna"=>true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
