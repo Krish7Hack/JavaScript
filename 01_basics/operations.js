3db48ca761ed738ea6bb818c29ea3e63e59352d0
Krish7Hack <145795573+Krish7Hack@users.noreply.github.com>
2024-05-30 08:06:32 +0000

    Operations in JavaScript

---
+++ 01_basics/operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//Operations
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello";
+let str2 = "krishna";
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);//1
+console.log(+"");//0
+
+let gameCounter=100;
+gameCounter++;//prefix and postfix operator precedence
+console.log(gameCounter);
